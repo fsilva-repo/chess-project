2c1eb6bb9900d90d2e15628456009b352ad7777a
fsilva <fsilva.py@gmail.com>
2026-08-01 00:51:44 -0300

    Criado as classes Board e Piece

--- v1/src/App.java
+++ v1/src/App.java
@@ -1,8 +1,14 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Position p = new Position(2, 6);
         System.out.println(p);
+
+        System.out.println();
+
+        Board board = new Board(8, 6);
+        System.out.println(board.getRows());
     }
 }
