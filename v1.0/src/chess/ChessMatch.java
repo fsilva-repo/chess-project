b50a4681094e3def2fdabd497a19307672fbc956
fsilva-repo <fsilva.py@gmail.com>
2026-09-14 01:11:48 -0300

    implementanto o bispo

--- v1.0/src/chess/ChessMatch.java
+++ v1.0/src/chess/ChessMatch.java
@@ -10,6 +10,7 @@ import boardGame.Position;
 import chessPieces.King;
 import chessPieces.Pawn;
 import chessPieces.Rook;
+import chessPieces.Bishop;
 import exception.ChessException;
 
 public class ChessMatch {
@@ -253,10 +254,10 @@ public class ChessMatch {
 
   placeNewPiece('a', 1, new Rook(board, Color.WHITE));
   //placeNewPiece('b', 1, new Knight(board, Color.WHITE));
-  //placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+  placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
   //placeNewPiece('d', 1, new Queen(board, Color.WHITE));
   placeNewPiece('e', 1, new King(board, Color.WHITE));
-  //placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+  placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
   //placeNewPiece('g', 1, new Knight(board, Color.WHITE));
   placeNewPiece('h', 1, new Rook(board, Color.WHITE));
   placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
@@ -270,10 +271,10 @@ public class ChessMatch {
 
   placeNewPiece('a', 8, new Rook(board, Color.BLACK));
   //placeNewPiece('b', 8, new Knight(board, Color.BLACK));
-  //placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+  placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
   //placeNewPiece('d', 8, new Queen(board, Color.BLACK));
   placeNewPiece('d', 8, new King(board, Color.BLACK));
-  //placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+  placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
   //placeNewPiece('g', 8, new Knight(board, Color.BLACK));
   placeNewPiece('h', 8, new Rook(board, Color.BLACK));
   placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
